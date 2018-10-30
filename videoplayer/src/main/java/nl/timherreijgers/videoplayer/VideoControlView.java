a7b575261642496ca156a753cdbbb1612f0bfb40
Tim Herreijgers <timtim3001@gmail.com>
2018-10-30 23:17:35 +0100

    Added pause button and darker alpha for background

--- videoplayer/src/main/java/nl/timherreijgers/videoplayer/VideoControlView.java
+++ videoplayer/src/main/java/nl/timherreijgers/videoplayer/VideoControlView.java
@@ -7,10 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class VideoControlView extends ConstraintLayout implements View.OnClickListener {
+class VideoControlView extends ConstraintLayout implements View.OnClickListener {
 
     private ImageButton playButton;
     private OnControlInteractedListener listener;
+    private boolean playing;
 
     public VideoControlView(Context context) {
         this(context, null);
@@ -25,16 +26,31 @@ public class VideoControlView extends ConstraintLayout implements View.OnClickLi
         LayoutInflater.from(context).inflate(R.layout.video_control_view, this);
         playButton = findViewById(R.id.playButton);
         playButton.setOnClickListener(this);
+        playing = true;
     }
 
     @Override
     public void onClick(View view) {
+        playing = !playing;
+        updatePlayButton();
         if(listener != null)
             listener.onPauseButtonClicked();
     }
 
+    private void updatePlayButton() {
+        if(playing)
+            playButton.setImageResource(R.drawable.play_arrow);
+        else
+            playButton.setImageResource(R.drawable.pause_buton);
+    }
+
     public void setListener(OnControlInteractedListener listener) {
         this.listener = listener;
+    }
+
+    public void setPlaying(boolean playing) {
+        this.playing = playing;
+        updatePlayButton();
     }
 
     public interface OnControlInteractedListener {
